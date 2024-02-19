f4d5f8d0509a97db87b589a97c38a77348e24de6
jdeng61 <jdeng61@gatech.edu>
2024-02-18 21:21:21 -0500

    adding map page and documentation

---
+++ sp24-react-app/sp24-react-app/src/app/map/page.js
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import Navbar from '../../pages/Navbar';
+
+export default function Page() {
+    return (
+        <div>
+            <Navbar />
+            <h2>Map</h2>
+        </div>
+    
+    )
+}
