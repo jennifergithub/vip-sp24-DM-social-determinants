139ea9a1db73a1506e470e4709073180df3ba3ec
jdeng61 <jdeng61@gatech.edu>
2024-02-18 21:16:45 -0500

    restarting from scratch, home and about pages

---
+++ sp24-react-app/sp24-react-app/src/app/page.js
@@ -0,0 +1,12 @@
+import Navbar from '../pages/Navbar';
+import EntryPoint from '../pages/EntryPoint';
+import React from 'react';
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <EntryPoint />
+    </>
+  );
+}
---
+++ sp24-react-app/sp24-react-app/src/pages/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'next/link'; // Import Link from Next.js
+import '../app/globals.css'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                <Link href="/about">About</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
